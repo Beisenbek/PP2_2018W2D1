bb0da72da933875e120fc4b998ce419871347be3
bsnbk <beysenbek@gmail.com>
2018-01-15 17:53:36 +0600

    upd

---
+++ Example3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student("A", "A", 2);
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ Example3/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example3
+{
+    class Student
+    {
+        string name;
+        string sname;
+        double gpa;
+        /// <summary>
+        ///  метод конструктор, который принимает начальные значения полей
+        /// </summary>
+        /// <param name="name">имя студента</param>
+        /// <param name="sname">фамилия студента</param>
+        /// <param name="gpa">ср.оценка</param>
+        public Student(string name, string sname, double gpa)
+        {
+            this.name = name;
+            this.sname = sname;
+            this.gpa = gpa;
+        }
+        public override string ToString()
+        {
+            return name + " " + sname + " " + gpa;
+        }
+    }
+}
